2ddd548b8e3920acb1e190869bb9266bfb85e51c
Developerkumar125 <Indaladevikumar@gmail.com>
2023-05-12 05:07:54 -0400

    tak me

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const cors = require("cors");
 
 const app = express();
 
-//var corsOptions = {
-  //origin: "https://frontend-app-gray.vercel.app/",
-//};
+var corsOptions = {
+  origin: "https://frontend-app-gray.vercel.app/",
+};
 
 // Enable CORS
-//app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // Parse requests with JSON payload
 app.use(express.json());
@@ -35,7 +35,10 @@ app.get("/", (req, res) => {
 // Include the tutorial routes
 require("./app/routes/turorial.routes")(app);
 
-
+// Handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).send("404: Page not found");
+});
 
 // Set the port and start the server
 const PORT = process.env.PORT || 8080;
