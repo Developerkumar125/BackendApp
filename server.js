eae421a0bf685af8ec6c392bdba68f79ee6981c7
Developerkumar125 <Indaladevikumar@gmail.com>
2023-05-12 05:03:11 -0400

    take time

--- server.js
+++ server.js
@@ -35,10 +35,7 @@ app.get("/", (req, res) => {
 // Include the tutorial routes
 require("./app/routes/turorial.routes")(app);
 
-// Handle 404 errors
-app.use((req, res, next) => {
-  res.status(404).send("404: Page not found");
-});
+
 
 // Set the port and start the server
 const PORT = process.env.PORT || 8080;
