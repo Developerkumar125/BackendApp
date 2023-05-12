bc352f2e8f7b635747a0cea9c492ef07793b8316
Developerkumar125 <Indaladevikumar@gmail.com>
2023-05-12 04:31:51 -0400

    last time

--- server.js
+++ server.js
@@ -1,47 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-
+const express = require('express');
 const app = express();
 
-var corsOptions = {
-  origin: "https://frontend-app-gray.vercel.app/",
-};
-
-// Enable CORS
-app.use(cors(corsOptions));
-
-// Parse requests with JSON payload
-app.use(express.json());
-
-// Parse URL-encoded requests
-app.use(express.urlencoded({ extended: true }));
-
-// Connect to the database
-const db = require("./app/models");
-db.sequelize
-  .sync()
-  .then(() => {
-    console.log("Synced db.");
-  })
-  .catch((err) => {
-    console.log("Failed to sync db: " + err.message);
-  });
-
-// Define a simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the application." });
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-// Include the tutorial routes
-require("./app/routes/turorial.routes")(app);
-
-// Handle 404 errors
-app.use((req, res, next) => {
-  res.status(404).send("404: Page not found");
-});
-
-// Set the port and start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server listening on port ${PORT}`);
 });
